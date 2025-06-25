46755cdb3c61026b85281320900193406dd68f10
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-25 16:07:20 +0700

    chore: Create Account uses form components

--- pages/create-account.vue
+++ pages/create-account.vue
@@ -14,87 +14,22 @@
       </v-btn>
     </v-card-title>
     <v-card-text class="mt-4">
-      <v-text-field
-        variant="solo"
-        flat
-        label="Email"
-        class="mb-4"
-        rounded="pill"
-        hide-details
-      ></v-text-field>
+      <!-- Email input-->
+      <FormEmail />
 
-      <v-text-field
-        variant="solo"
-        flat
-        label="Password"
-        class="mb-4"
-        rounded="pill"
-        hide-details
-        :append-inner-icon="showPasswd ? 'mdi-eye' : 'mdi-eye-off'"
-        @click:append="showPasswd = !showPasswd"
-        :type="showPasswd ? 'text' : 'password'"
-      ></v-text-field>
+      <!-- Password input-->
+      <FormPassword />
 
-      <v-text-field
-        variant="solo"
-        flat
-        label="Your Number"
-        class="mb-4"
-        rounded="pill"
-        hide-details
-      >
-        <template #prepend-inner>
-          <v-menu offset="15" class="">
-            <template v-slot:activator="{ props }">
-              <v-btn
-                variant="text"
-                class="border-e-md"
-                tile
-              >
-                <v-avatar
-                  v-bind="props"
-                  :image="selectedFlag"
-                  size="x-small"
-                  class="mr-2"
-                >
-                </v-avatar>
-                <v-icon>mdi-chevron-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="locale in LOCALE"
-                :key="locale.value"
-                @click="selectedLocale = locale.value"
-              >
-                <template v-slot:prepend>
-                  <v-avatar
-                    :image="locale.flag"
-                    size="x-small"
-                  />
-                </template>
-                <v-list-item-title class="text-body-2">
-                  {{ locale.label }}
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </template>
-      </v-text-field>
+      <!-- Phone number input-->
+      <FormPhoneNumber />
     </v-card-text>
 
     <v-spacer></v-spacer>
     <v-card-actions class="justify-center d-flex flex-column">
-      <v-btn
-        color="primary"
-        variant="flat"
-        class="text-none rounded-xl"
-        width="100%"
-        size="x-large"
-        :to="{ name: 'index' }"
-      >
-        <span class="btn-text">Done</span>
-      </v-btn>
+      <FormBigButton
+        :to="{ name: 'login' }"
+        title="Done"
+      />
 
       <v-list-item class="text-black">
         <template #title>
@@ -108,45 +43,6 @@
     </v-card-actions>
   </v-card>
 </template>
-
-<script setup lang="ts">
-const showPasswd = ref(false);
-
-const selectedLocale = ref('en'); // Default locale
-
-const LOCALE = [
-  {
-    label: "English",
-    value: "en",
-    flag: "https://flagcdn.com/w40/us.png",
-  },
-  {
-    label: "French",
-    value: "fr",
-    flag: "https://flagcdn.com/w40/fr.png",
-  },
-  {
-    label: "Spanish",
-    value: "es",
-    flag: "https://flagcdn.com/w40/es.png",
-  },
-  {
-    label: "German",
-    value: "de",
-    flag: "https://flagcdn.com/w40/de.png",
-  },
-  {
-    label: "Chinese",
-    value: "cn",
-    flag: "https://flagcdn.com/w40/cn.png",
-  }
-]
-
-const selectedFlag = computed(() => {
-  const locale = LOCALE.find((l) => l.value === selectedLocale.value);
-  return locale ? locale.flag : '';
-});
-</script>
 
 <style scoped>
 .wrapper {
