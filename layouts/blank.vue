901a8f0422744c4f13c7908685d799ed94cf4b81
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-25 13:17:05 +0700

    feat: update Blank layout

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,8 +1,35 @@
 <template>
   <v-app>
     <v-main>
-      <v-container class="fill-height">
-        <slot />
+      <v-container class="fill-height ma-0 pa-0">
+        <v-row no-gutters class="align-center justify-center">
+          <v-col cols="12" md="12" class="text-center">
+            <v-card
+              class="bg-transparent elevation-0 py-8 d-flex flex-column"
+              min-height="100vh"
+            >
+              <!-- Top section with auto margin -->
+              <v-spacer></v-spacer>
+              
+              <!-- Middle section (centered) -->
+              <div>
+                <slot name="content">
+                  Default content goes here
+                </slot>
+              </div>
+              
+              <!-- Spacer to push actions to bottom -->
+              <v-spacer></v-spacer>
+              
+              <!-- Bottom actions -->
+              <v-card-actions class="justify-center d-flex flex-column">
+                <slot name="actions">
+                  Default actions go here
+                </slot>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,38 @@
 <template>
-  <v-row no-gutters>
-    <v-col cols="12" class="text-center ma-0 pa-0">
+  <NuxtLayout name="blank">
+    <template #content>
+      <!-- Content goes in the "content" slot -->
       <div class="logo-container rounded-circle elevation-12">
         <v-img src="/public/logo.svg" height="90" width="80" class=""></v-img>
       </div>
+
       <v-card-title class="title-h1 my-4">Shoppe</v-card-title>
-      <v-card-text class="text-subtitle-1 text-wrap w-75 mx-auto mb-10">
+      <v-card-text class="text-body-1 text-wrap w-75 mx-auto">
         Beautiful eCommerce UI Kit for your online store
       </v-card-text>
-      <v-card-actions class="justify-center d-flex flex-column justify-center">
-        <v-btn color="primary" variant="flat" class="text-none rounded-xl" width="80%" size="x-large">
-          <span class="btn-text">Let's get started</span>
-        </v-btn>
-        <v-list-item class="text-black">
-          <template #title>
-            <span class="text-subtitle-2">I already have an account</span>
-          </template>
-          <template #append>
-            <v-btn color="primary" class="text-none ml-4" icon variant="flat" size="x-small">
-              <v-icon>mdi-arrow-right</v-icon>
-            </v-btn>
-          </template>
-        </v-list-item>
-      </v-card-actions>
-    </v-col>
-  </v-row>
+    </template>
+
+    <template #actions>
+      <!-- Bottom actions go in the "action" slot -->
+      <v-btn color="primary" variant="flat" class="text-none rounded-xl" width="80%" size="x-large">
+        <span class="btn-text">Let's get started</span>
+      </v-btn>
+      <v-list-item class="text-black">
+        <template #title>
+          <span class="text-subtitle-2">I already have an account</span>
+        </template>
+        <template #append>
+          <v-btn color="primary" class="text-none ml-4" icon variant="flat" size="x-small">
+            <v-icon>mdi-arrow-right</v-icon>
+          </v-btn>
+        </template>
+      </v-list-item>
+    </template>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-definePageMeta({
-  layout: 'blank',
-  title: 'Welcome to Shoppe',
-  meta: [
-    { name: 'description', content: 'Shoppe - Beautiful eCommerce UI Kit for your online store' },
-    { name: 'keywords', content: 'eCommerce, UI Kit, Shoppe, Online Store' }
-  ]
-})
+
 </script>
 
 <style scoped>
