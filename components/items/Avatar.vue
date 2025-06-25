b9d78291ae038a749f88b91a4197c89a47b39642
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-25 23:13:16 +0700

    feat: Avatar component

---
+++ components/items/Avatar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="avatar-frame">
+    <v-avatar
+      size="91"
+      image="/public/images/avatar.png"
+    >
+    </v-avatar>
+  </div>
+</template>
+<style scoped>
+.avatar-frame {
+  width: 105px;
+  height: 105px;
+  border-radius: 50%;
+  background-color: #FFFFFF;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
