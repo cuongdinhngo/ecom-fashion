97dbab78b6ca00712ed9163342e1a71f4e6d10d4
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-25 13:34:14 +0700

    fix: Blank layout & Start page

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -2,32 +2,9 @@
   <v-app>
     <v-main>
       <v-container class="fill-height ma-0 pa-0">
-        <v-row no-gutters class="align-center justify-center">
-          <v-col cols="12" md="12" class="text-center">
-            <v-card
-              class="bg-transparent elevation-0 py-8 d-flex flex-column"
-              min-height="100vh"
-            >
-              <!-- Top section with auto margin -->
-              <v-spacer></v-spacer>
-              
-              <!-- Middle section (centered) -->
-              <div>
-                <slot name="content">
-                  Default content goes here
-                </slot>
-              </div>
-              
-              <!-- Spacer to push actions to bottom -->
-              <v-spacer></v-spacer>
-              
-              <!-- Bottom actions -->
-              <v-card-actions class="justify-center d-flex flex-column">
-                <slot name="actions">
-                  Default actions go here
-                </slot>
-              </v-card-actions>
-            </v-card>
+        <v-row no-gutters>
+          <v-col cols="12" md="12" class="pa-0 ma-0">
+            <slot />
           </v-col>
         </v-row>
       </v-container>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,35 @@
 <template>
-  <NuxtLayout name="blank">
-    <template #content>
-      <!-- Content goes in the "content" slot -->
+  <v-card
+    class="bg-transparent elevation-0 py-8 d-flex flex-column px-0"
+    min-height="100vh"
+  >
+    <v-spacer></v-spacer>
+
+    <div>
       <div class="logo-container rounded-circle elevation-12">
         <v-img src="/public/logo.svg" height="90" width="80" class=""></v-img>
       </div>
 
-      <v-card-title class="title-h1 my-4">Shoppe</v-card-title>
-      <v-card-text class="text-body-1 text-wrap w-75 mx-auto">
+      <v-card-title class="title-h1 my-4 text-center">Shoppe</v-card-title>
+      <v-card-text class="text-body-1 text-wrap w-75 mx-auto text-center">
         Beautiful eCommerce UI Kit for your online store
       </v-card-text>
-    </template>
+    </div>
 
-    <template #actions>
-      <!-- Bottom actions go in the "action" slot -->
-      <v-btn color="primary" variant="flat" class="text-none rounded-xl" width="80%" size="x-large">
+    <v-spacer></v-spacer>
+
+    <v-card-actions class="justify-center d-flex flex-column">
+      <v-btn
+        color="primary"
+        variant="flat"
+        class="text-none rounded-xl"
+        width="100%"
+        size="x-large"
+        :to="{ name: 'create-account' }"
+      >
         <span class="btn-text">Let's get started</span>
       </v-btn>
+
       <v-list-item class="text-black">
         <template #title>
           <span class="text-subtitle-2">I already have an account</span>
@@ -27,12 +40,11 @@
           </v-btn>
         </template>
       </v-list-item>
-    </template>
-  </NuxtLayout>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped>
