144aed09b180628ba54fd6015a57fde868bb78bb
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-26 14:50:42 +0700

    feat: Icon Action components

---
+++ components/form/IconBtnText.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-badge v-if="showBadge" color="primary" dot offset-y="10">
+    <v-btn
+      variant="plain" color="primary" :icon="icon"
+      :to="to"
+      :size="size"
+      active-color="primary"
+    >
+      <v-icon :icon="icon" :size="size"></v-icon>
+    </v-btn>
+  </v-badge>
+  <v-btn v-else
+    variant="plain" color="primary" :icon="icon"
+    :to="to"
+    :size="size"
+    active-color="primary"
+  >
+    <v-icon :icon="icon" :size="size"></v-icon>
+  </v-btn>
+
+</template>
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: [String, Object],
+    default: { naem: 'index' }
+  },
+  icon: {
+    type: String,
+    default: 'mdi-wallet-giftcard'
+  },
+  showBadge: {
+    type: Boolean,
+    default: true
+  },
+  size: {
+    type: [String, Number],
+    default: 'default'
+  }
+});
+</script>
---
+++ components/form/IconRipple.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-icon
+    :icon="icon"
+    :size="size"
+    class="pa-8 rounded-circle"
+    :color="color"
+    v-ripple
+    @click="navigateTo(to)"
+  ></v-icon>
+</template>
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: [String, Object],
+    default: { name: 'index' }
+  },
+  icon: {
+    type: String,
+    default: 'mdi-wallet-giftcard'
+  },
+  size: {
+    type: [String, Number],
+    default: 'default'
+  },
+  color: {
+    type: String,
+    default: 'primary'
+  }
+});
+</script>
