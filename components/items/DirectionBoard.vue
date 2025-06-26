c917e2139d4965e07da4268cebc2782560d6bd71
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-26 14:51:28 +0700

    feat: Direction Board component

---
+++ components/items/DirectionBoard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-list class="rounded-lg" :lines="false">
+    <v-list-item>
+      <template #title>
+        <h4>{{ title }}</h4>
+      </template>
+      <template #subtitle>
+        <p class="text-caption text-wrap">{{ description }}</p>
+      </template>
+      <template #append>
+        <v-btn
+          icon="mdi-chevron-right"
+          variant="flat"
+          color="primary"
+          :to="to"
+        ></v-btn>
+      </template>
+    </v-list-item>
+  </v-list>
+</template>
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: 'Anouncement'
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  to: {
+    type: [String, Object],
+    default: () => ({ name: 'index' })
+  }
+});
+</script>
