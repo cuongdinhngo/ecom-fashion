053c1c320c1a0389712732bbb7f80f75fcab9730
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-26 14:51:43 +0700

    feat: Top Data Card component

---
+++ components/items/TopDataCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card class="elevation-0 bg-transparent my-3">
+  <v-card-title class="text-h6 font-weight-bold px-0">
+    {{ title }}
+  </v-card-title>
+  <v-card-text class="d-flex justify-space-between px-0">
+    <slot />
+  </v-card-text>
+</v-card>
+</template>
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: 'Top Data'
+  }
+});
+</script>
