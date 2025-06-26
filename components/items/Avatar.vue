2b1fe2dcf9bca7689e231ab95415e9ae74a597b1
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-26 14:50:59 +0700

    chore: Avatar props

--- components/items/Avatar.vue
+++ components/items/Avatar.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="avatar-frame">
+  <div
+    class="avatar-frame elevation-1"
+    :style="{ width: frameSize.width, height: frameSize.height }"
+  >
     <v-avatar
-      size="91"
-      image="/public/images/avatar.png"
+      :size="size"
+      :image="avatar"
     >
     </v-avatar>
   </div>
 </template>
+<script setup lang="ts">
+defineProps({
+  size: {
+    type: [String, Number],
+    default: 91
+  },
+  avatar: {
+    type: String,
+    default: imagePath('avatar.png')
+  },
+  frameSize: {
+    type: Object,
+    default: {
+      width: '105px',
+      height: '105px'
+    }
+  }
+});
+</script>
 <style scoped>
 .avatar-frame {
-  width: 105px;
-  height: 105px;
   border-radius: 50%;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   display: flex;
   align-items: center;
   justify-content: center;
