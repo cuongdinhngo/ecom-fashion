c4e8e2813f541b149b50e7f9f5d12045cfed0a83
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-26 14:52:03 +0700

    feat: profile layout

---
+++ layouts/profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <!-- AppBar for Profile Layout -->
+    <SectionsProfileAppBar />
+
+    <v-main>
+      <v-container>
+        <v-row no-gutters>
+          <v-col cols="12" md="12" class="pa-0 ma-0">
+            <slot />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <!-- Footer for Profile Layout -->
+    <SectionsProfileFooter />
+  </v-app>
+</template>
