ee2c8ba96bf4e671ff41f0c6d75e478ed497b601
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-26 14:52:42 +0700

    feat: Footer of profile layout

---
+++ components/sections/profile/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-footer
+    app
+    class="d-flex justify-space-between align-center border-t-thin"
+  >
+    <FormIconRipple
+      icon="mdi-wallet-giftcard"
+      size="x-large"
+      :to="{ name: 'index' }"
+    />
+
+    <FormIconRipple
+      icon="mdi-heart-outline"
+      size="x-large"
+      :to="{ name: 'index' }"
+    />
+
+    <FormIconRipple
+      icon="mdi-list-box-outline"
+      size="x-large"
+      :to="{ name: 'index' }"
+    />
+
+    <FormIconRipple
+      icon="mdi-shopping-outline"
+      size="x-large"
+      :to="{ name: 'index' }"
+    />
+
+    <FormIconRipple
+      icon="mdi-account-circle-outline"
+      size="x-large"
+      :to="{ name: 'index' }"
+    />
+  </v-footer>
+</template>
