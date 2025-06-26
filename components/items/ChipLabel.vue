528c875486239d2ea2f97b5f4a554b7a04da0887
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-26 14:51:15 +0700

    feat: Chip Lable component

---
+++ components/items/ChipLabel.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-badge
+    v-if='showBadge'
+    dot
+    offset-x="10"
+    :color="badgeColor"
+  >
+    <v-chip
+      :color="chipColor"
+      :size="size"
+    >{{ label }}</v-chip>
+  </v-badge>
+  <v-chip
+    v-else
+    :color="chipColor"
+    :size="size"
+  >{{ label }}</v-chip>
+</template>
+<script setup lang="ts">
+defineProps({
+  showBadge: {
+    type: Boolean,
+    default: false
+  },
+  badgeColor: {
+    type: String,
+    default: 'primary'
+  },
+  size: {
+    type: String,
+    default: 'x-large'
+  },
+  chipColor: {
+    type: String,
+    default: 'primary'
+  },
+  label: {
+    type: String,
+    default: ''
+  }
+});
+</script>
