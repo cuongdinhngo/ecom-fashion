c4c9a1a1df5bbb17755999bfae0d9780c3031536
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-27 17:46:53 +0700

    feat: Discount Products

---
+++ components/sale/DiscountProducts.vue
@@ -0,0 +1,29 @@
+<template>
+  <ItemsDiscountProductCard
+    :discount="discount"
+  >
+  <v-row no-gutters>
+    <v-col
+      v-for="(item, index) in products"
+      :key="index"
+      cols="6"
+    >
+      <ItemsProductCard
+        :item="item"
+      />
+    </v-col>
+    </v-row>
+  </ItemsDiscountProductCard>
+</template>
+<script setup lang="ts">
+defineProps({
+  discount: {
+    type: Number,
+    default: 20
+  },
+  products: {
+    type: Array,
+    default: () => []
+  }
+});
+</script>
