76f27fbd7ad5cb28c5ebd4b1b125229d8276cb03
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-27 12:33:38 +0700

    feat: Default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <v-container class="fill-height ma-0 pa-0">
+      <v-container>
         <v-row no-gutters>
           <v-col cols="12" md="12" class="pa-0 ma-0">
             <slot />
@@ -9,5 +9,8 @@
         </v-row>
       </v-container>
     </v-main>
+
+    <!-- Footer for Profile Layout -->
+    <SectionsProfileFooter />
   </v-app>
 </template>
