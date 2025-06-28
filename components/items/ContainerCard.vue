71e489facbf723f1ea622846bfbe89cb61dbf14f
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-28 22:10:22 +0700

    feat: Container Card

---
+++ components/items/ContainerCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="elevation-0 bg-transparent my-3">
+    <!-- Card Header -->
+    <v-card-title
+      v-if="!hiddenHeader"
+      class="d-flex justify-space-between px-0"
+    >
+      <!-- Header Left -->
+      <div class="header-left">
+        <slot name="header-left"></slot>
+      </div>
+
+      <!-- Header Center -->
+      <div class="header-right">
+        <slot name="header-right"></slot>
+      </div>
+    </v-card-title>
+
+    <!-- Card Content -->
+    <v-card-text class="d-flex justify-space-between px-0">
+      <slot />
+    </v-card-text>
+  </v-card>
+</template>
+<script setup lang="ts">
+defineProps({
+  hiddenHeader: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
