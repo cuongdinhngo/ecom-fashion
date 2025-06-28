e5c66a3624d051347e972ba682e285d52400be2e
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-28 22:10:36 +0700

    feat: Header Title

---
+++ components/items/HeaderTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <span class="text-h6 font-weight-bold d-flex align-center">
+    {{ title }}
+    <v-icon :color="icon?.color" :size="icon?.size" class="ml-2">{{ icon?.name }}</v-icon>
+  </span>
+</template>
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: 'Header Title'
+  },
+  icon: {
+    type: Object,
+    default: () => ({ name: null, color: null, size: null })
+  }
+});
+</script>
