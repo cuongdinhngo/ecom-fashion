b2f7c80181ca56ee04b3fd080dab270223b11a99
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-30 14:37:04 +0700

    feat: Dialog Filter

--- components/sections/shop/AppBar.vue
+++ components/sections/shop/AppBar.vue
@@ -7,7 +7,8 @@
 
     <v-spacer></v-spacer>
 
-    <div class="search-bar">
+    <!-- Search Bar -->
+    <div class="search-bar d-flex align-center">
       <v-autocomplete
         v-model="searchQuery"
         :items="searchTerms"
@@ -15,7 +16,7 @@
         variant="solo-filled"
         rounded="pill"
         menu-icon=""
-        width="320"
+        :width="searchQuery.length === 0 ? 320 : 290"
         placeholder="Search ..."
         item-title="title"
         item-value="title"
@@ -51,12 +52,272 @@
           ></v-list-item>
         </template>
       </v-autocomplete>
+
+      <v-btn
+        v-if="searchQuery.length > 0"
+        variant="text"
+        icon
+        size="small"
+        @click="dialog = !dialog"
+      >
+        <v-icon>mdi-tune-vertical</v-icon>
+      </v-btn>
     </div>
   </v-app-bar>
+  
+  <!-- Dialog for Filter -->
+  <v-dialog
+    v-model="dialog"
+    transition="dialog-bottom-transition"
+    fullscreen
+  >
+    <v-card class="bg-white">
+      <v-toolbar class="bg-white">
+        <v-toolbar-title class="text-h5">Filter</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn
+          icon="mdi-close"
+          @click="dialog = false"
+        ></v-btn>
+      </v-toolbar>
+
+      <!-- Category List -->
+      <v-card-title
+        v-if="subCategories.length > 0"
+        class="mt-0 py-0"
+      >
+        <v-item-group selected-class="bg-primary" multiple>
+          <v-row no-gutters>
+            <v-col
+              v-for="(item, index) in subCategories"
+              :key="index"
+              cols="2"
+              class="d-flex flex-column align-center justify-center"
+            >
+              <v-item v-slot="{ isSelected, selectedClass, toggle }">
+                <div
+                  class="avatar-frame elevation-1"
+                  :style="{ width: '60px', height: '60px', position: 'relative' }"
+                >
+                  <v-avatar
+                    :size="50"
+                    :image="item.image"
+                    @click="toggle()"
+                  ></v-avatar>
+                  
+                  <!-- Check Signal-->
+                  <v-avatar
+                    v-if="isSelected"
+                    size="20"
+                    color="primary"
+                    style="position: absolute;top: 2px; right: 2px;"
+                  >
+                    <v-icon size="10">mdi-check-bold</v-icon>
+                  </v-avatar>
+                </div>
+                <v-list-item-title class="text-caption text-center text-wrap mt-1" style="height: 36px;">
+                  {{ item.title }}
+                </v-list-item-title>
+              </v-item>
+            </v-col>
+          </v-row>
+        </v-item-group>
+      </v-card-title>
+
+      <!-- Size Filter -->
+      <v-card-title class="d-flex justify-space-between align-center">
+        <span class="text-h6 font-weight-bold">Size</span>
+        <div class="selected-categories">
+          <v-chip
+            v-for="(category, index) in searchQuery"
+            :key="index"
+            class="mr-2"
+            color="primary"
+            variant="outlined"
+          >
+            {{ category }}
+          </v-chip>
+        </div>
+      </v-card-title>
+      <v-card-title>
+        <v-slider
+          color="primary"
+          :max="5"
+          :ticks="SIZE_OPTIONS"
+          show-ticks="always"
+          :thumb-size="20"
+          tick-size="0"
+          step="1"
+        ></v-slider>
+      </v-card-title>
+
+      <!-- Color Filter -->
+      <v-card-title class="d-flex justify-space-between align-center">
+        <span class="text-h6 font-weight-bold">Color</span>
+      </v-card-title>
+      <v-card-title class="d-flex justify-space-between align-center">
+        <v-slide-group
+          v-model="selectedColor"
+          center-active
+          :show-arrows="false"
+        >
+          <v-slide-group-item
+            v-for="item in colors"
+            :key="item"
+            v-slot="{ isSelected, toggle }"
+          >
+            <div
+              class="frame elevation-1 mr-3"
+              style="width: 50px; height: 50px; position: relative;"
+            >
+              <span
+                class="color-picker" :style="{backgroundColor: item.color}" @click="toggle"
+              ></span>
+              <v-avatar
+                v-if="isSelected"
+                size="20"
+                color="primary"
+                style="position: absolute; top: 2px; right: 2px;"
+              >
+                <v-icon size="10">mdi-check-bold</v-icon>
+              </v-avatar>
+            </div>
+          </v-slide-group-item>
+        </v-slide-group>
+      </v-card-title>
+
+      <!-- Price filter -->
+      <v-card-title class="d-flex justify-space-between align-center">
+        <span class="text-h6 font-weight-bold">Price</span>
+        <div class="price-range">
+          <span class="min-price">${{ range[0] }}</span> - <span class="min-price">${{ range[1] }}</span>
+        </div>
+      </v-card-title>
+      <v-card-title>
+        <v-range-slider
+          v-model="range"
+          :max="100"
+          :min="1"
+          :step="1"
+          class="align-center"
+          hide-details
+          color="primary"
+        >
+        </v-range-slider>
+      </v-card-title>
+
+      <!--Quick filter-->
+      <v-card-title>
+        <v-item-group selected-class="text-primary font-weight-bold">
+          <v-row no-gutters>
+            <v-col
+              v-for="(filter, index) in quickFilters"
+              :key="index"
+              cols="6"
+              class="ma-0 pa-0 pa-1"
+            >
+              <v-item v-slot="{ isSelected, selectedClass, toggle }">
+                <v-list-item
+                  @click="toggle(); selectedQuickFilter = filter.value"
+                  rounded="pill"
+                  :class="[
+                    isSelected ? 'bg-blue-lighten-5' : 'bg-grey-lighten-3', 'text-center',
+                    selectedClass
+                  ]"
+                >
+                  <template #title>
+                    <span :class="isSelected ? 'font-weight-bold' : ''">{{  filter.label }}</span>
+                  </template>
+
+                  <template #append>
+                    <v-btn
+                      v-if="isSelected"
+                      icon
+                      variant="flat"
+                      color="primary"
+                      size="x-small"
+                    ><v-icon>mdi-check-bold</v-icon></v-btn>
+                  </template>
+                </v-list-item>
+              </v-item>
+            </v-col>
+          </v-row>
+        </v-item-group>
+      </v-card-title>
+
+      <v-card-actions class="d-flex justify-center pa-6">
+        <v-btn
+          variant="outlined"
+          color="primary"
+          class="text-none rounded-xl"
+          @click="dialog = false"
+          width="30%"
+          size="large"
+        >
+          Cancel
+        </v-btn>
+        <v-btn
+          variant="flat"
+          color="primary"
+          class="text-none rounded-xl"
+          @click="dialog = false"
+          width="70%"
+          size="large"
+        >
+          Apply Filters
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 <script setup lang="ts">
 const searchQuery = useSearchQuery();
 
-const { SEARCH_TERMS, CATEGORIES } = useProducts();
+const { SEARCH_TERMS, CATEGORIES, SIZE_OPTIONS, searchSubCategories } = useProducts();
 const searchTerms = ref<Object[]>(CATEGORIES);
+const dialog = ref(false);
+const subCategories = ref<any[]>([]);
+const size = ref(1);
+const selectedColor = ref<string>('grey');
+const colors = [
+  { color: 'grey' },
+  { color: 'red' },
+  { color: 'blue' },
+  { color: 'green' },
+  { color: 'yellow' },
+  { color: 'purple' },
+  { color: 'orange' },
+  { color: 'pink' }
+];
+
+const range = ref([1, 100]);
+
+const quickFilters = ref([
+  { label: 'Popular', value: 'popular' },
+  { label: 'Newest', value: 'newest' },
+  { label: 'Price Low to High', value: 'price_low' },
+  { label: 'Price High to Low', value: 'price_high' }
+]);
+const selectedQuickFilter = ref('');
+
+watch(searchQuery, (newValue) => {
+  console.log('Search Query Updated:', newValue);
+  subCategories.value = searchSubCategories(newValue);
+  console.log('subCategories Updated:', subCategories.value);
+});
 </script>
+<style scoped>
+.frame {
+  border-radius: 50%;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.color-picker {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+</style>
