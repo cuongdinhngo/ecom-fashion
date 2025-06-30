ec07fea3ad613d74442d054037e270322d62f78a
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-30 23:00:34 +0700

    feat: Wishlist navigation

--- components/sections/profile/Footer.vue
+++ components/sections/profile/Footer.vue
@@ -13,7 +13,7 @@
 <script setup lang="ts">
 const footerMenu = [
   { icon: 'mdi-wallet-giftcard', to: { name: 'shop' }, page: 'shop', size: 'x-large', color: 'primary' },
-  { icon: 'mdi-heart-outline', to: { name: 'index' }, page: 'index', size: 'x-large', color: 'primary' },
+  { icon: 'mdi-heart-outline', to: { name: 'wishlist' }, page: 'wishlist', size: 'x-large', color: 'primary' },
   { icon: 'mdi-list-box-outline', to: { name: 'index' }, page: 'index', size: 'x-large', color: 'primary' },
   { icon: 'mdi-shopping-outline', to: { name: 'index' }, page: 'index', size: 'x-large', color: 'primary' },
   { icon: 'mdi-account-circle-outline', to: { name: 'profile' }, page: 'profile', size: 'x-large', color: 'primary' },
