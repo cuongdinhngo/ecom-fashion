4f67a06f9edcdc6bdd8b671933cc70874b59dca0
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-30 11:05:25 +0700

    feat: shop layout

---
+++ layouts/shop.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <!-- AppBar for Shop Layout -->
+    <SectionsShopAppBar />
+
+    <!-- main Content for Shop Layout -->
+    <v-main>
+      <v-container>
+        <v-row no-gutters>
+          <v-col cols="12" md="12" class="pa-0 ma-0">
+            <slot />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <!-- Footer for Shop Layout -->
+    <SectionsProfileFooter />
+  </v-app>
+</template>
+
+<script setup lang="ts">
+</script>
