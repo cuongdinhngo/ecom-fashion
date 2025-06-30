745935aa7149c756820f2afe164aa789d569860c
Cuong Ngo <dinhcuongngo@gmail.com>
2025-06-30 17:55:55 +0700

    feat: product layout

---
+++ layouts/product.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="ma-0 pa-0">
+        <v-row no-gutters>
+          <v-col cols="12" md="12" class="pa-0 ma-0">
+            <slot />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
