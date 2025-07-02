6e6aa3c34186f655c858ee40dd56bd5c93129484
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-02 16:33:23 +0700

    feat: Track Order page

---
+++ composables/useTracker.ts
@@ -0,0 +1,65 @@
+import { faker } from "@faker-js/faker";
+
+const deliverySteps = [
+  {
+    status: 'Packed',
+    value: 0,
+    description: 'Your order has been packed and is ready for delivery.',
+    timestamp: useDateFormat(faker.date.past(), 'MMM, DD HH:mm'),
+  },
+  {
+    status: 'On the Way to the Logistic Facility',
+    value: 1,
+    description: 'Your order is on the way to the logistic facility.',
+    timestamp: useDateFormat(faker.date.past(), 'MMM, DD HH:mm'),
+  },
+  {
+    status: 'Arrived at the Logistic Facility',
+    value: 2,
+    description: 'Your order has arrived at the logistic facility.',
+    timestamp: useDateFormat(faker.date.past(), 'MMM, DD HH:mm'),
+  },
+  {
+    status: 'Out for Delivery',
+    value: 3,
+    description: 'Your order is out for delivery.',
+    timestamp: useDateFormat(faker.date.past(), 'MMM, DD HH:mm'),
+  },
+  {
+    status: 'Shipping',
+    value: 4,
+    description: 'Your order is being shipped.',
+    timestamp: useDateFormat(faker.date.past(), 'MMM, DD HH:mm'),
+  },
+];
+
+const deliveredStep =   {
+  status: 'Delivered',
+  value: 5,
+  description: 'Your order has been delivered.',
+  timestamp: useDateFormat(faker.date.past(), 'MMM, DD HH:mm'),
+};
+
+const attempToDeliver =   {
+  status: 'Attemp to Deliver',
+  value: 5,
+  description: 'An attempt to deliver your order was made but no one was available to receive it.',
+  timestamp: useDateFormat(faker.date.past(), 'MMM, DD HH:mm'),
+};
+
+export const useTracker = () => {
+  const getTrackerSteps = (status: string) => {
+    switch (status) {
+      case 'delivered':
+        return [...deliverySteps, deliveredStep];
+      case 'attempt-to-deliver':
+        return [...deliverySteps, attempToDeliver];
+      default:
+        return deliverySteps;
+    }
+  }
+    
+  return {
+    getTrackerSteps,
+  }
+}
---
+++ pages/profile/track-order.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- Track Stepper -->
+  <ProfileTrackStepper
+    :step="currentStep"
+  />
+
+  <v-list-item class="bg-grey-lighten-3 px-2 py-1 mt-4 mb-6" rounded="lg">
+    <template #title>
+      <span class="text-subtitle-1 font-weight-bold">Tracking Number</span>
+    </template>
+    <template #subtitle>
+      <span class="text-body-2">#123456789</span>
+    </template>
+    <template #append>
+      <v-btn variant="text" color="primary">
+        <v-icon>mdi-content-copy</v-icon>
+      </v-btn>
+    </template>
+  </v-list-item>
+
+  <ProfileStepperStatusCard
+    v-for="step in steps"
+    :key="step.value"
+    :stepper="step"
+  />
+</template>
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'profile',
+  pageTitle: 'To Receive',
+  pageDetail: 'Track Your Order',
+})
+
+const { getTrackerSteps } = useTracker();
+const steps = getTrackerSteps('delivered');
+
+const currentStep = computed(() => {
+  const lastStep = steps[steps.length - 1].value;
+  if (lastStep < 1) {
+    return 0;
+  } else if (lastStep >= 1 && lastStep < 4) {
+    return 1;
+  } else {
+    return 2
+  }
+});
+</script>
