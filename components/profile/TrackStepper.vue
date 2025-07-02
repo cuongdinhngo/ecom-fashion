ce7d76365e0f895d17cf196ecd1d718bfc28fa15
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-02 16:33:12 +0700

    feat: Track Stepper component

---
+++ components/profile/TrackStepper.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card-title class="px-1">
+    <v-slider
+      :max="2"
+      :ticks="milestones"
+      :model-value="step"
+      prepend-icon="mdi-shopping-outline"
+      append-icon="mdi-home-account"
+      show-ticks="always"
+      tick-size="15"
+      color="primary"
+      readonly
+      elevation="5"
+    ></v-slider>
+  </v-card-title>
+</template>
+<script lang="ts" setup>
+defineProps({
+  step: {
+    type: Number,
+    default: 0,
+  },
+})
+const milestones = {
+  0: 'Shop',
+  1: 'Shipping',
+  2: 'You',
+}
+</script>
+<style>
+.v-slider-track__tick {
+  border-radius: 50% !important;
+}
+</style>
