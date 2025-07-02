af29189bea881a35ca467d71aba26b36e1f1fafe
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-02 10:52:58 +0700

    feat: To Receive page

---
+++ pages/profile/to-receive.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 4 images -->
+  <ProfileOrderCard
+    v-for="(order, index) in orders"
+    :key="index"
+    :order="order"
+  >
+    <template #imgGrid>
+      <ProfileFourImgGrid
+        v-if="order.images && order.images.length >= 4"
+      />
+      <ProfileThreeImgGrid
+        v-else-if="order.images && order.images.length === 3"
+      />
+
+      <ProfileTwoImgGrid
+        v-else-if="order.images && order.images.length === 2"
+      />
+
+      <ProfileOneImgGrid
+        v-else-if="order.images && order.images.length === 1"
+      />
+    </template>
+  </ProfileOrderCard>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'profile',
+  title: 'To Receive | My Orders',
+  description: 'View your orders that are to be received.',
+  pageTitle: 'To Receive',
+  pageDetail: 'My Orders',
+});
+
+const { orders } = useOrders({ quantity: 5 });
+</script>
