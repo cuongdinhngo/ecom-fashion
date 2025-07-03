169a29ad1dd64cdd0b78199cf678ecda7c7171e0
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-03 11:04:07 +0700

    chore: product layout is set fluid

--- layouts/product.vue
+++ layouts/product.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <v-container class="ma-0 pa-0">
+      <v-container class="ma-0 pa-0" fluid>
         <v-row no-gutters>
-          <v-col cols="12" md="12" class="pa-0 ma-0">
+          <v-col cols="12" md="12" lg="12" class="pa-0 ma-0">
             <slot />
           </v-col>
         </v-row>
