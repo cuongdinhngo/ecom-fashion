82eca7dbb5d15ee309e3107e89c54e88157f9e2b
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-06 22:27:31 +0700

    feat: Product Description component

---
+++ components/items/ProductDescription.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product-description">
+    <p class="text-caption">{{ description }}</p>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  description: {
+    type: String,
+    required: true,
+    default: ''
+  }
+});
+</script>
+<style scoped>
+.product-description {
+  font-size: 1.2rem;
+  min-height: 40px;
+  overflow: hidden;
+  margin-top: 5px
+}
+
+.product-description p {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin: 0;
+}
+</style>
