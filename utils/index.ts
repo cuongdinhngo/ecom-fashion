148881f4850da2d7dab21e893766c8619c159881
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-15 09:31:02 +0700

    fix: image path

--- utils/index.ts
+++ utils/index.ts
@@ -1,25 +1,25 @@
 import { faker } from "@faker-js/faker";
 
 export function imagePath(image: string): string {
-  return `${useRuntimeConfig().app.baseURL}/images/${image}`;
+  return `/images/${image}`;
 }
 
 export function avatarImg(): string {
-  return `${useRuntimeConfig().app.baseURL}/images/avatar-${faker.number.int({min: 1, max: 8})}.jpg`;
+  return `/images/avatar-${faker.number.int({min: 1, max: 8})}.jpg`;
 }
 
 export function bannerImg(): string {
-  return `${useRuntimeConfig().app.baseURL}/images/banner-${faker.number.int({min: 1, max: 6})}.jpg`;
+  return `/images/banner-${faker.number.int({min: 1, max: 6})}.jpg`;
 }
 
 export function categoryImg(): string {
-  return `${useRuntimeConfig().app.baseURL}/images/c-${faker.number.int({min: 1, max: 20})}.jpg`;
+  return `/images/c-${faker.number.int({min: 1, max: 20})}.jpg`;
 }
 
 export function smallProductImg(): string {
-  return `${useRuntimeConfig().app.baseURL}/images/item-${faker.number.int({min: 1, max: 13})}.jpg`;
+  return `/images/item-${faker.number.int({min: 1, max: 13})}.jpg`;
 }
 
 export function productImg(): string {
-  return `${useRuntimeConfig().app.baseURL}/images/product-${faker.number.int({min: 1, max: 7})}.jpg`;
+  return `/images/product-${faker.number.int({min: 1, max: 7})}.jpg`;
 }
