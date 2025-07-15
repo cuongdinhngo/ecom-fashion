1e29e633a951c6e6e17c9b83394d9b87dbac7ce0
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-15 16:06:06 +0700

    fix: Recently Viewed all

--- components/profile/RecentlyViewed.vue
+++ components/profile/RecentlyViewed.vue
@@ -1,14 +1,30 @@
 <template>
-  <ItemsTopDataCard title="Recently Viewed">
-    <ItemsAvatar
-      v-for="(item, index) in recentlyViews"
-      :key="index"
-      :avatar="item.avatar"
-      :size="60"
-      :frame-size="{ width: '75px', height: '70px' }"
-      @click="navigateTo(item.to)"
-    />
-  </ItemsTopDataCard>
+  <!-- Recently Viewed -->
+  <v-card-text class="pa-0">
+    <ItemsContainerCard>
+      <template #header-left>
+        <span class="text-h6 font-weight-bold">Recently Viewed</span>
+      </template>
+      <template #header-right>
+        <v-btn
+          variant="flat"
+          size="x-small"
+          icon="mdi-arrow-right"
+          color="primary"
+          :to="{ name: 'wishlist-recently-viewed' }"
+        >
+        </v-btn>
+      </template>
+      <ItemsAvatar
+        v-for="(item, index) in recentlyViews"
+        :key="index"
+        :avatar="item.avatar"
+        :size="60"
+        :frame-size="{ width: '75px', height: '70px' }"
+        @click="navigateTo({ name: 'product-id', params: { id: index + 1 } })"
+      />
+    </ItemsContainerCard>
+  </v-card-text>
 </template>
 <script setup lang="ts">
 import { faker } from '@faker-js/faker';
--- pages/wishlist/index.vue
+++ pages/wishlist/index.vue
@@ -4,31 +4,8 @@
   </v-card-title>
 
   <!-- Recently Viewed -->
-  <v-card-text class="pa-0">
-    <ItemsContainerCard>
-      <template #header-left>
-        <span class="text-h6 font-weight-bold">Recently Viewed</span>
-      </template>
-      <template #header-right>
-        <v-btn
-          variant="flat"
-          size="x-small"
-          icon="mdi-arrow-right"
-          color="primary"
-          :to="{ name: 'wishlist-recently-viewed' }"
-        >
-        </v-btn>
-      </template>
-      <ItemsAvatar
-        v-for="(item, index) in recentlyViews"
-        :key="index"
-        :avatar="item.avatar"
-        :size="60"
-        :frame-size="{ width: '75px', height: '70px' }"
-        @click="navigateTo({ name: 'product-id', params: { id: index + 1 } })"
-      />
-    </ItemsContainerCard>
-  </v-card-text>
+  <ProfileRecentlyViewed
+  />
 
   <!-- Wishlist -->
   <v-card-text :class="['pa-0', {'blank-container': items.length === 0}]">
